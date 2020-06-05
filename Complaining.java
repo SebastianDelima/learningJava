e2e93fa40d5396e3e66b88ca2b66511d8d9be775
Sebastian Delima <sebastiandelimagec@gmail.com>
2020-06-05 14:35:51 -0400

    adde

--- Complaining.java
+++ Complaining.java
@@ -1,10 +1,29 @@
+
+
 public class Complaining{
     
     public static void main(String[] args) {
 
-        char n = 'n';
+        double decimal = 24.27364678d;
 
-         System.out.println(n);
+        long hugeNumber = 23467822234622222L;
+
+        short onethousand = 1000;
+
+        boolean yes = true;
+        boolean no  = false;
+
+
+        int one = 1;
+        int two = 2;
+        int three = 3;
+
+
+        char a = 'A'; //A
+        char A = 65; //A
+
+         System.out.println(decimal);
+         System.out.println(no);
     }
    
 }
